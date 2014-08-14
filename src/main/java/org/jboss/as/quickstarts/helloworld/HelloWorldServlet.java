6318ca0e85894c4a7062bbb2e1700c2c15df84fc
Jake2Mi <user21-08@mail.ru>
2014-08-14 11:51:51 +0300

    Update HelloWorldServlet.java

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -42,6 +42,10 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 @WebServlet("/HelloWorld")
 public class HelloWorldServlet extends HttpServlet {
+    
+     public static void test() {
+        System.out.println("test");
+    }
 
     static String PAGE_HEADER = "<html><head><title>helloworld</title></head><body>";
 
